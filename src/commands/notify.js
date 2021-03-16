86bdb273cca29e9448c29a556df728f1b8147a74
coo_coo1 <github@chokanov.com>
2021-03-16 14:35:36 +0000

    feat: Add most functionality of notify command It will take other commands and args and store them in a json, the time command has a special condition for an arg, put in basic clear and list options as well

--- src/commands/notify.js
+++ src/commands/notify.js
@@ -5,56 +5,148 @@ const config = require('../../res/bot-config.json');
 
 module.exports = {
 	name: 'notify',
-	description: `You can pass the notify command any other command that returns a list (baro, fissure, etc.)` + 
-	`along with it valid arguments for that command and you will be signed up to recieve a DM from the bot whenever that filtered event occurs.` +
-	`The ${config.prefix}notify time command differs where you have to pass it a cycle to be notified for and can pass it X minutes(as a number) after the [location] argument to recieve the DM that many minutes beforehand`,
+	description: `You can pass the notify command any other command that returns a list (baro, fissure, etc.) ` + 
+	`along with it valid arguments for that command and you will be signed up to recieve a DM from the bot whenever that filtered event occurs.\n\n` +
+	`The **${config.prefix}notify time** command differs where you have to pass it a cycle to be notified for and can pass it X minutes(as a number) ` +
+	`after the [location] argument to recieve the DM that many minutes beforehand.\n\n` +
+	`use **${config.prefix}notify list** to get a DM of all your currently set notification, and **${config.prefix}notify clear** to clear them all`,
 	aliases: ['not', 'watch', 'alert'],
 	usage: `${config.prefix}notify [command name] [command arguments] {time cycle} {minutes till time cycle becomes active}`,
 	example: `${config.prefix}notify fissure axi rescue, ${config.prefix}notify baro, ${config.prefix}notify time cetus night 10`,
 	cooldown: 5,
 
-	execute(message, args) {
+	execute(message, args) {	//TODO currently allows duplicate entries
 		if (!args.length) {
             return message.reply(`\n soon(tm)`)
         }
-		let commandsList = [], notificationType = null;
+		let commandsList = [], notifyCommand = null;
 
 		for (const file of fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'))) {
 			const command = require(`./${file}`);
 			commandsList.push(command);
 		}
+
+		commandsList.push({name:'list'})
+		commandsList.push({name:'clear'})
+
 		
 		for(command of commandsList){
 			if (command.name == args[0]){
-				notificationType = command;
+				notifyCommand = command;
 				break;
 			}
 			for (i in command.aliases){
 				if (command.aliases[i] == args[0]){
-					notificationType = command;
+					notifyCommand = command;
 					break;
 				}
 			}
 		}
 
-		if(notificationType == null){
+		if(notifyCommand == null){
 			return message.reply(`'${args[0]}' is not a valid argument for a command. Please use ${config.prefix}help notify for more information`)
 		}
 
-		switch(notificationType.name) {
+		let argsFound;
+		switch(notifyCommand.name) {
 			case('notify'):	//TODO - something special
 				return message.channel.send(`\`ha ha\``)
 
 			case('time'):
+				return timeNotification(message, notifyCommand, args)
+			case('help'):
 				return message.reply('maybe some other time')
+
+			case('list'):
+				return listNotification(message);
+			
+			case('clear'):
+				return clearNotification(message);
+				
 			default:
-				args.splice(0,1)
-				let argsFound = commons.valiateArgs(args, notificationType.validArgs, config.validArgsData);
-				if(argsFound.invalid.length){message.reply(`\nthe argument(s) '${argsFound.invalid}' are unknown. They will be skipped`)}
-				//TODO write to file
-				console.log(`Notify args: type-${notificationType.name}, other args-${JSON.stringify(argsFound.valid)}`)
-			break;
+				return defaultNotification(message, notifyCommand, args);
 		}
 
 	},
 };
+
+function defaultNotification(message, notifyCommand, args){
+	args.splice(0,1)
+	argsFound = commons.valiateArgs(args, notifyCommand.validArgs, config.validArgsData);
+	if(argsFound.invalid.length){message.reply(`\nthe argument(s) '${argsFound.invalid}' are unknown. They will be skipped`)}
+	
+	console.log(`Notify args: type-${notifyCommand.name}, other args-${JSON.stringify(argsFound.valid)}`)
+	if(!addNotification(message.author.id, notifyCommand.name, argsFound.valid)){
+		return message.reply('there was an issue completing your request. You know who to blame.')
+	}
+	return message.reply(`\n You've been signed up for a notification whenever a **${notifyCommand.name}** ` +	//TODO less debugy reply
+	`with these argument(s) appears:\n${JSON.stringify(argsFound.valid)}`)
+}
+
+function timeNotification(message, notifyCommand, args){
+	args.splice(0,1)
+	argsFound = commons.valiateArgs(args, notifyCommand.validArgs, config.validArgsData);
+	for(i in argsFound.invalid){	//TODO multiple int args are ignored aside from the first valid one
+		if (parseInt(argsFound.invalid[i]) !== NaN){
+			if(argsFound.valid.time === undefined){argsFound.valid.time = parseInt(argsFound.invalid[i])}
+			argsFound.invalid.splice(i,1)
+			break;
+		}
+	}
+
+	if(argsFound.invalid.length){message.reply(`\nthe argument(s) '${argsFound.invalid}' are unknown. They will be skipped`)}
+	
+	console.log(`Notify args: type-${notifyCommand.name}, other args-${JSON.stringify(argsFound.valid)}`)
+	if(!addNotification(message.author.id, notifyCommand.name, argsFound.valid)){
+		return message.reply(' there was an issue completing your request. You know who to blame.')
+	}
+	return message.reply(`\n You've been signed up for a notification whenever a **${notifyCommand.name}** ` +	//TODO less debugy reply
+	`with these argument(s) appears:\n${JSON.stringify(argsFound.valid)}`)
+}
+
+function clearNotification(message){
+	try {
+		fs.unlinkSync(`./res/subs/${message.author.id}.json`);
+	} catch (error) {
+		console.log(error);
+		return message.reply(' there was an issue completing your request. You know who to blame.')
+	}
+	return message.reply(' your list is cleared.')
+}
+
+function listNotification(message){
+	fs.readFile(`./res/subs/${message.author.id}.json`, (error, data) => {
+		if (error) {
+			console.log(error);
+			return message.reply(' there was an issue completing your request. You know who to blame.')
+		}
+		let userData = JSON.parse(data) 
+		let reply = [];
+		for (i in userData){
+			reply.push(`\ncommand: ${userData[i].command} args: ${JSON.stringify(userData[i].args)}`)
+		}
+		console.log(reply)
+		return message.reply(`Here's a list of your current notifications:${reply}`,{split: true})
+	});
+	return true; 
+}
+
+function addNotification(userId, commandName, commandArgs){
+	fs.readFile(`./res/subs/${userId}.json`, (error, data) => {
+		if (error) {
+			console.log(`${userId}.json doesn't exist or can't be opened. Making a new one.`)
+		} 
+		let subData = []
+		if (data !== undefined){ subData = JSON.parse(data) }
+
+		subData.push(JSON.parse(`{"command": "${commandName}", "args": ${JSON.stringify(commandArgs)}}`))
+
+		try { 
+			fs.writeFileSync(`./res/subs/${userId}.json`, JSON.stringify(subData)); 
+		} catch(err) { 
+			console.error(err); 
+			return false
+		}
+	});
+	return true; 
+}
